c8973f03efeb00ed1abfe79baf888c5d897816e9
sangboking <cowg001@naver.com>
2022-09-27 15:26:55 +0900

    fix : 몽고디비 비밀번호 .env 파일로 분류

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sanghoon:653659@express-ex.bucejoc.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('몽고 연결 성공'))
+mongoose.connect(`mongodb+srv://sanghoon:${process.env.MONGODB_PASSWORD}@express-ex.bucejoc.mongodb.net/?retryWrites=true&w=majority`).then(() => console.log('몽고 연결 성공'))
   .catch(err => console.log(err))
 
 
